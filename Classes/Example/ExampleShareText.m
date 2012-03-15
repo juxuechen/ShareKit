b0ceff8555f2ae069cf9bff2c014b62b1c48d0dc
icyleaf <icyleaf.cn@gmail.com>
2012-03-15 21:13:49 +0800

    Updated zh_CN localizable

--- Classes/Example/ExampleShareText.m
+++ Classes/Example/ExampleShareText.m
@@ -59,7 +59,7 @@
 	self.textView = [[[UITextView alloc] initWithFrame:CGRectMake(0,0,self.view.bounds.size.width,self.view.bounds.size.height)] autorelease];
 	[self.view addSubview:textView];
 	
-	textView.text = @"This is a chunk of text.  If you highlight it, you'll be able to share the selection.  If you tap the share button below, it will share all of it.";
+	textView.text = SHKLocalizedString(@"This is a chunk of text.  If you highlight it, you'll be able to share the selection.  If you tap the share button below, it will share all of it.");
 	textView.editable = NO;
 }
 
